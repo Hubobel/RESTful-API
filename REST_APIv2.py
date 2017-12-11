4fb57f17b923b6bf0694922f4eda3338eafcd58f
hubobel <carsten@hubobel.de>
2017-12-11 11:20:11 +0100

    die Methode /wetter liefert aktuelle Wetter in Oggersheim zurück

--- REST_APIv2.py
+++ REST_APIv2.py
@@ -213,6 +213,7 @@ def index():
     <p>GET: api.hubobel.de/lotto/aktuell.....: Liefert die letzten Lottozahlen des Euro- und Mittwoch 
     bzw.Samstagslotto (online jeweils neu ermittelt)</p>
     <p>GET: api.hubobel.de/bitcoin.....: Liefert die aktuellen Bitcoinkurse in EURO</p>
+    <p>GET: api.hubobel.de/wetter.....: Liefert das aktuelle Wetter in Oggersheim</p>
     <p>POST: api.hubobel.de/lotto/6aus49/check.....: Uebergabe der 6+1 Zahlen als Liste - liefert Anzahl
      der Treffer zurueck</p>
     <p>POST: api.hubobel.de/lotto/6aus49/check.....: Uebergabe der 5+2 Zahlen als Liste - liefert Anzahl 
@@ -351,7 +352,15 @@ def bitcoin():
     resp_zitat = req.get(url_zitat)
     data_zitat = resp_zitat.json()
     return jsonify(data_zitat['EUR'])
-
+@app.route('/wetter',methods=['GET'])
+def wetter():
+    url = 'http://api.wunderground.com/api/35a8e37c649985d5/conditions/lang:DL/q/Germany/pws:ILUDWIGS227.json'
+    response = req.get(url)
+    data_response = response.json()
+    temperatur = (data_response['current_observation']['temp_c'])
+    wetter = (data_response['current_observation']['weather'])
+    feuchte = (data_response['current_observation']['relative_humidity'])
+    return jsonify({'Temperatur':temperatur,'Wetter':wetter,'Feuchte':feuchte})
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
 
