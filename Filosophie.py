8c00d98377748f36dc15de36f1eba88216f7c815
hubobel <carsten@hubobel.de>
2018-08-05 15:57:48 +0200

    icke bin widda dohhh

--- Filosophie.py
+++ Filosophie.py
@@ -2,9 +2,39 @@
 import bs4 as bs
 import requests
 import random
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
 ergebniss=''
 
+def Nachricht(sub='I am ROOT',body='this comes from Hubobel'):
+    fromaddr = 'schneeschieben@web.de'
+    toaddr = 'carsten.richter@soka-bau.de'
+    bccs = toaddr
+
+    pwd = 'PL19zPL19z'
+    acc = 'carsten.richter77@gmail.com'
+
+    msg = MIMEMultipart()
+
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = sub
+
+    msg.attach(MIMEText(body, 'plain'))
+
+
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(acc, pwd)
+    text = msg.as_string()
+    server.sendmail(fromaddr, bccs, text)
+    server.quit()
+    return
 
 requests.packages.urllib3.disable_warnings()
 sauce = requests.get('https://www.swr3.de/wraps/fun/filosofie/neu.php?id=11', verify=False)
@@ -23,3 +53,5 @@ soup = bs.BeautifulSoup(sauce.content,'lxml')
 for i in soup.find_all('strong'):
     filosophie=(i.text)
 print(filosophie)
+
+Nachricht(filosophie,filosophie)
